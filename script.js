48ad7274fd3ada598d2422a61e24c8ac67bac5d2
munreden <85946284+munreden@users.noreply.github.com>
2025-10-08 18:00:41 +0200

    Added default tag

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const travelDate = '2025-09-01T09:00:00';
+const travelDate = '2050-01-01T09:00:00';
 const printedTravelDate = printTravelDate(travelDate);
-const city = 'Guardamar del Segura';
+const city = 'Ciudad Misteriosa';
 const countdownDate = new Date(travelDate).getTime();
 const title = `¡Próximo viaje a ${city}!`;
 
